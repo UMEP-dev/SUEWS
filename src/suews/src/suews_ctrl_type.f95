fffdb2123aef3acc3d958b88b620e141279ffa80
Matthew <m.paskin@pgr.reading.ac.uk>
2025-06-03 14:51:43 +0100

    Fixed type definitions for ss radiation

--- src/suews/src/suews_ctrl_type.f95
+++ src/suews/src/suews_ctrl_type.f95
@@ -912,10 +912,10 @@ MODULE SUEWS_DEF_DTS
       REAL(KIND(1D0)), DIMENSION(15) :: wall_in_sw_spc ! incoming shortwave radiation on wall surface [W m-2]
       REAL(KIND(1D0)), DIMENSION(15) :: wall_in_lw_spc ! incoming longwave radiation on wall surface [W m-2]
 
-      REAL(KIND(1D0)) :: roof_in_sw_spc ! incoming shortwave radiation on roof surface [W m-2]
-      REAL(KIND(1D0)) :: roof_in_lw_spc ! incoming longwave radiation on roof surface [W m-2]
-      REAL(KIND(1D0)) :: wall_in_sw_spc ! incoming shortwave radiation on wall surface [W m-2]
-      REAL(KIND(1D0)) :: wall_in_lw_spc ! incoming longwave radiation on wall surface [W m-2]
+      REAL(KIND(1D0)), DIMENSION(15) :: roof_in_sw_spc ! incoming shortwave radiation on roof surface [W m-2]
+      REAL(KIND(1D0)), DIMENSION(15) :: roof_in_lw_spc ! incoming longwave radiation on roof surface [W m-2]
+      REAL(KIND(1D0)), DIMENSION(15) :: wall_in_sw_spc ! incoming shortwave radiation on wall surface [W m-2]
+      REAL(KIND(1D0)), DIMENSION(15) :: wall_in_lw_spc ! incoming longwave radiation on wall surface [W m-2]
 
       ! flag for iteration safety - YES
       ! all variables are intensive and thus can be used for iteration safety

cb1dd0a3318b15027549621bd31f7602b5efe231
HarroJongen <59564863+HarroJongen@users.noreply.github.com>
2025-10-27 13:11:44 +0100

    Update configuration from dictionaries
    When the trying to change parameters below the list-like sites level, the nested dictionary approach could not handle the input. The function is extended to check when a list-like parameter is accessed whether (in this order):
    1 An integer is provided, which is used as an index for the list.
    2 A string is provided that matches one of the site names.
    3 The list has a length of only one.

--- src/supy/suews_sim.py
+++ src/supy/suews_sim.py
@@ -121,17 +121,37 @@ class SUEWSSimulation:
 
     def _update_config_from_dict(self, updates: dict):
         """Apply dictionary updates to configuration."""
-
         def recursive_update(obj, upd):
             for key, value in upd.items():
                 if hasattr(obj, key):
                     attr = getattr(obj, key)
                     if isinstance(value, dict) and hasattr(attr, "__dict__"):
-                        # Recursive update for nested objects
+                        # Recursive to read nested dictionairies
                         recursive_update(attr, value)
                     else:
-                        setattr(obj, key, value)
-
+                        # Check whether site index or site name is provided
+                        if isinstance(attr, list):
+                            site_key = list(value.keys())[0]
+                            site_value = value[site_key]
+                            if type(site_key)==int:
+                                # Select site on index
+                                attr = attr[site_key]
+                                recursive_update(attr, site_value)
+                            elif type(site_key)==str:
+                                # Select site on name
+                                attr_site = next((item for item in attr if item.name == site_key), None)
+                                if attr_site:
+                                    recursive_update(attr_site, site_value)
+                                elif len(attr)==1:
+                                    # Without name or index and only one site
+                                    attr_site = attr[0]
+                                    recursive_update(attr_site, value)
+                                else:
+                                    # Otherwise skip these parameters
+                                    continue
+                        else:
+                            setattr(obj, key, value)
+    
         recursive_update(self._config, updates)
 
     def update_forcing(self, forcing_data):

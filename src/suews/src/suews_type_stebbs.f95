631229646950797ef9ca35fa6f6c5057e7221c56
yiqing1021 <dg829684@student.reading.ac.uk>
2026-01-27 20:45:32 +0000

    normalise DHW heat flux output

--- src/suews/src/suews_type_stebbs.f95
+++ src/suews/src/suews_type_stebbs.f95
@@ -143,8 +143,8 @@ module module_type_stebbs
       REAL(KIND(1D0)) :: QHload_cooling_tstepFA = 0.0D0
       REAL(KIND(1D0)) :: QH_metabolism = 0.0D0
       REAL(KIND(1D0)) :: QE_metabolism = 0.0D0
-      REAL(KIND(1D0)) :: Qtotal_water_tank = 0.0D0
-      REAL(KIND(1D0)) :: qhwtDrain = 0.0D0
+      REAL(KIND(1D0)) :: Qtotal_water_tank_tstepFA = 0.0D0
+      REAL(KIND(1D0)) :: Qloss_drain_tstepFA = 0.0D0
       REAL(KIND(1D0)) :: ratio_window_wall = 0.0D0
       REAL(KIND(1D0)) :: Afootprint = 0.0D0
       REAL(KIND(1D0)) :: height_building = 0.0D0

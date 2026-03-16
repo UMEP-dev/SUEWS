ddadf2b5b2c490d3cdfe0b5d7a933c3655c6ab47
Silvia Rognone <rognone.silvia@gmail.com>
2026-03-16 17:09:46 +0000

    Update test_output_config.py

--- test/io_tests/test_output_config.py
+++ test/io_tests/test_output_config.py
@@ -158,7 +158,6 @@ model:
     assert config.model.control.output_file == "output.txt"
 
 
-
 def test_deprecation_warning():
     """Test deprecation warning for non-default string output_file"""
     import warnings

c0c4861e8e9aeec294453e7894aa121fcc859c9e
Silvia Rognone <71443948+dayantur@users.noreply.github.com>
2026-03-20 12:08:07 +0000

    Feat: improve STEBBS parameters descriptions (#1257)
    * improve External wall area description
    
    * improve stebbs_Height description
    
    * improve FootprintArea
    
    * Update CHANGELOG.md

--- src/supy/data_model/core/site.py
+++ src/supy/data_model/core/site.py
@@ -1298,19 +1298,19 @@ class ArchetypeProperties(BaseModel):
 
     stebbs_Height: Optional[FlexibleRefValue(float)] = Field(
         default=10.0,
-        description="Building height [m]",
+        description="Building height. This should be consistent with WallExternalArea and FootprintArea. [m]",
         json_schema_extra={"unit": "m", "display_name": "Stebbs Height"},
         gt=0.0,
     )
     FootprintArea: Optional[FlexibleRefValue(float)] = Field(
         default=64.0,
-        description="Building footprint area [m2]",
+        description="Building footprint area. This should be consistent with stebbs_Height and WallExternalArea. [m2]",
         json_schema_extra={"unit": "m^2", "display_name": "Footprint Area"},
         gt=0.0,
     )
     WallExternalArea: Optional[FlexibleRefValue(float)] = Field(
         default=80.0,
-        description="External wall area (including window area) [m2]",
+        description="External wall area (including window area). This should be consistent with stebbs_Height and FootprintArea. [m2]",
         json_schema_extra={"unit": "m^2", "display_name": "Wall External Area"},
         gt=0.0,
     )
